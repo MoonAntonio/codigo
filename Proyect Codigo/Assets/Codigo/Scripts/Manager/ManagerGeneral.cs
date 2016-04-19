8e410e31b08a269e955d0e29827d7837e1403088
lPinchol <pinchodev91@gmail.com>
2016-04-19 05:06:47 +0200

    Add manager general and manager save

---
+++ Proyect Codigo/Assets/Codigo/Scripts/Manager/ManagerGeneral.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManagerGeneral : MonoBehaviour
+{
+    void Awake()
+    {
+        // Dont destroy this gameobject never.
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Proyect Codigo/Assets/Codigo/Scripts/Manager/ManagerSave.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManagerSave : MonoBehaviour
+{
+    public static int _intCopiler;
+    public static int _intBug;
+
+    [Range(0.0f,999.9f)]
+    public int intCopiler;
+    [Range(0.0f, 999.9f)]
+    public int intBug;
+
+    public void Update()
+    {
+        intCopiler = _intCopiler;
+        intBug = _intBug;
+    }
+}
